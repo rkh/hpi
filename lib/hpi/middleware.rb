84e44f6ca2e31841263b44e664caf120e1209361
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-10-13 17:52:12 -0700

    silence rack logger

--- lib/hpi/middleware.rb
+++ lib/hpi/middleware.rb
@@ -3,14 +3,20 @@ require 'hpi'
 module HPI
   # This middleware is used to check if the server is running.
   class Middleware
+    @@null    = File.open('/dev/null', 'w')
+    @@counter = 0
+
     def initialize(app)
       @app = app
     end
 
     def call(env)
+      env['rack.errors'] = @@null
       if env['PATH_INFO'] == '/__hpi__'
         [200, {'Content-Type' => 'text/plain'}, ['ok']]
       else
+        @@counter = (@@counter + 1) % 50
+        $stderr.print('.') if @@counter == 0
         @app.call(env)
       end
     end
