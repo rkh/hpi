f18ab08d93c466fcc2a794f13ad7f87c2c562bfc
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-10-28 12:08:09 -0700

    rewrite tracking middleware

--- lib/hpi/middleware.rb
+++ lib/hpi/middleware.rb
@@ -1,24 +1,50 @@
 require 'hpi'
+require 'set'
 
 module HPI
-  # This middleware is used to check if the server is running.
   class Middleware
-    @@null    = File.open('/dev/null', 'w')
-    @@counter = 0
+    include HPI::Lock
 
     def initialize(app)
-      @app = app
+      @app        = app
+      @last_entry = nil
+      @last_time  = Time.at(0)
+      @bursts     = Hash.new { |h,k| h[k] = Set.new }
     end
 
     def call(env)
-      env['rack.errors'] = @@null
-      if env['PATH_INFO'] == '/__hpi__'
-        [200, {'Content-Type' => 'text/plain'}, ['ok']]
-      else
-        @@counter = (@@counter + 1) % 50
-        $stderr.print('.') if @@counter == 0
-        @app.call(env)
+      case env['HTTP_X_HPI']
+      when nil, ''  then lock { record(env) }
+      when 'status' then lock { status(env) }
+      end or @app.call(env)
+    end
+
+    def path(env)
+      path = env["SCRIPT_NAME"].to_s + env["PATH_INFO"].to_s
+      path << "?" << path["QUERY_STRING"] if path["QUERY_STRING"] and not path["QUERY_STRING"].empty?
+      path
+    end
+
+    def record(env)
+      return unless env['REQUEST_METHOD'] == 'GET'
+
+      lock do
+        @last_time + 1 >= Time.now ?
+          @bursts[@last_entry] << path(env) :
+          @last_entry = path(env)
+        result = @app.call(env)
+        @last_time = Time.now
+        result
       end
+    end
+
+    def status(env)
+      status = lock do
+        info = SystemInfo.to_hash.merge 'bursts' => @bursts, 'server' => env['SERVER_SOFTWARE'], 'path' => path(env)
+        OkJson.encode("system" => SystemInfo.to_hash, "rack" => info(env), "bursts" => @bursts)
+      end
+
+      [ 200, { 'Content-Type' => 'application/json', 'Content-Length' => status.bytesize }, status ]
     end
   end
 end
